3f05031d0e74d40b108d2133fa6ced5a3cc7778b
Arnav <arnav@Arnavs-MacBook-Pro.local>
2017-08-07 19:17:19 +0530

    update file

--- Example/AVFonts/AppDelegate.swift
+++ Example/AVFonts/AppDelegate.swift
@@ -18,8 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        AVFonts.changeFont(toFont: "Avenir-Heavy", increment: 0)
-        AVFonts.swapFont(currentFont: <#T##String#>, toFont: <#T##String#>)
+        AVFonts.changeFont(toFont: "Avenir-Heavy")
         AVFonts.applyAVFonts()
         
      //   UIFont.systemFont(ofSize: <#T##CGFloat#>)
