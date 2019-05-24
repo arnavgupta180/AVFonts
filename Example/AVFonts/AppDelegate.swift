8fff311b88dff6822dd6755d3153bdbe89269e1e
Arnav Gupta <arnavgupta180@gmail.com>
2019-05-24 08:09:29 +0530

    remove linter error

--- Example/AVFonts/AppDelegate.swift
+++ Example/AVFonts/AppDelegate.swift
@@ -11,10 +11,7 @@ import AVFonts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         AVFonts.changeFont(toFont: "Avenir-Heavy")
@@ -43,7 +40,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
--- Example/AVFonts/ViewController.swift
+++ Example/AVFonts/ViewController.swift
@@ -9,51 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-//MARK:- IBOutlets
+    // MARK: - IBOutlets
     @IBOutlet weak var tfName: UITextField!
-    
     @IBOutlet weak var tfEmail: UITextField!
-    
     @IBOutlet weak var tfPassword: UITextField!
-   
     @IBOutlet weak var btnLogin: UIButton!
-
-    
-}
-
-//MARK:-
-//MARK:- VC LifeCycle  functions
-extension ViewController{
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
-    }
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-    }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(true)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
